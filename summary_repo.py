04b1b830190e2a8ccf6631967a5c76bb6cae0d32
Vokturz <victornavarrokp47@gmail.com>
2023-05-24 18:05:51 -0400

    first commit

---
+++ summary_repo.py
@@ -0,0 +1,120 @@
+import inquirer
+import requests
+import argparse
+import os
+from dotenv import load_dotenv
+
+
+import glob
+import numpy as np
+from langchain.embeddings import HuggingFaceEmbeddings
+import utils
+load_dotenv()
+
+embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL')
+
+
+def user_repo_validation(answers, current):
+    if '/' not in current:
+        raise inquirer.errors.ValidationError("", reason="You must write the user and repo separated by '/'")
+    current_split = current.split('/')
+    if len(current_split) != 2:
+        raise inquirer.errors.ValidationError("", reason="Incorrect format, there is more than one '/'")
+    user, repo = current_split
+    if len(user)==0: 
+        raise inquirer.errors.ValidationError("", reason="user cannot be empty")
+    elif len(repo)==0:
+        raise inquirer.errors.ValidationError("", reason="repository cannot be empty")
+    if github_token:
+        headers = {'Authorization': f'token {github_token}'}
+        response = requests.get(f'https://api.github.com/repos/{user}/{repo}/branches', headers=headers)
+    
+    else:    
+        response = requests.get(f'https://github.com/{user}/{repo}')
+    if response.status_code != 200:
+        raise inquirer.errors.ValidationError("", reason="This repository does not exist or is private.\nCheck that the GitHub token is correctly defined")
+    return True
+
+
+
+
+def main():
+    args = parse_arguments()
+
+    if not args.local:
+        repo_question = inquirer.Text('user_repo', message="GitHub repository (user/repo)", validate=user_repo_validation, default='idsudd/derco-cjourney'),             
+        user_repo = inquirer.prompt(repo_question)['user_repo']
+        # user_n_repo = input('GitHub repository: ') or 'idsudd/fic-trips-and-ods'
+        user, repo = user_repo.split('/')
+        
+        branches = utils.get_branches(user, repo, github_token)
+        default = ['main', 'master']
+        questions = [inquirer.List(
+            'branch',
+            message="Select the branch that you want to summarize",
+            choices=branches,
+            default=default
+        )]
+        answers = inquirer.prompt(questions)
+        branch = answers['branch']
+        print(f'Clonning {user_repo} ({branch})..')
+        repo_folder = utils.clone_repository(user, repo, branch, token=github_token)
+    
+    else:
+        repo_folder = args.local
+        
+    if not os.path.exists(repo_folder):
+        print(f'There is no {repo_folder} folder')
+
+
+    source_directory = repo_folder
+    repo_name = repo_folder.split('/')[-1]
+
+    ipynb_files = glob.glob(os.path.join(repo_folder, f"**/[!_]*.ipynb"), recursive=True)
+    parent_folders = ['/'.join(file_path.split('/')[1:-1]) for file_path in ipynb_files]
+    unique_parent_folders, total_documents = np.unique(parent_folders, return_counts=True)
+    unique_parent_folders = [pf.replace(f'{repo_name}/', '') for pf in unique_parent_folders]
+    ipynb_count = []
+    
+    for p, t in zip(unique_parent_folders,total_documents):
+       
+        ipynb_count.append(f' {t} files from {p}')
+    q = [inquirer.Checkbox("selected_parent_folders", message="Select .ipynb folder files to summarize",
+                           choices=tuple(zip(ipynb_count, unique_parent_folders )),
+                           default=unique_parent_folders)]
+    answers = inquirer.prompt(q)
+    selected_parent_folders = answers["selected_parent_folders"]
+    if len(selected_parent_folders) == 0:
+        print('No folder was selected')
+        return
+    documents = []
+    for spf in selected_parent_folders:
+        source_dir = f'{source_directory}/{spf}'
+        documents.extend(utils.load_multiple_notebooks(source_dir))
+    
+    print(f"Loaded {len(documents)} documents from {source_directory}")
+
+    print(f'Loading embeddings from {embeddings_model_name}..')
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+
+    llm, results_pf = utils.retrieve_summary(documents, embeddings, 
+                                             model_type=args.model, print_token_n_costs=True)
+    summary_notebooks = utils.format_summary(results_pf, repo_name)
+    summary_repo = utils.summary_repo(llm, summary_notebooks, repo_name, 
+                                      model_type=args.model, print_token_n_costs=True)
+    readme = utils.create_readme(repo_name, summary_notebooks, summary_repo)
+    with open(f'{source_directory}/notebooks_summary.md', 'w') as f:
+        f.write(readme)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='A description')
+    parser.add_argument("--local", "-l", help='If the repository has been already downloaded, you can pass the folder path')
+    parser.add_argument("--model", "-m", default='FakeLLM', help='To use a preferred model (OpenAI, FakeLLM for testing, GPT4ALL)')
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    github_token = os.environ.get("GITHUB_TOKEN")
+    if not os.path.exists('repositories/'):
+        os.makedirs('repositories/')
+    main()
---
+++ utils.py
@@ -0,0 +1,167 @@
+import os
+from git import (Repo, Git)
+import nbformat
+import glob
+from dotenv import load_dotenv
+from typing import List, Any
+from langchain.docstore.document import Document
+import requests
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.chains import RetrievalQA
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import GPT4All, OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.llms.fake import FakeListLLM
+load_dotenv()
+
+model_path = os.environ.get("GPT4_ALL_MODEL")
+
+
+def load_notebook(file_path: str) -> Document:
+    if file_path.endswith('.ipynb'):
+        with open(file_path) as fp:
+            notebook = nbformat.read(fp, nbformat.NO_CONVERT)
+            cells = notebook['cells']
+    full_source = []
+    for cell in cells: 
+        if cell['cell_type'] == 'markdown': # Add Markdown as comments
+            lines = cell['source'].splitlines()
+            commented_lines = ['# ' + line for line in lines]
+            full_source.append('\n'.join(commented_lines))
+        else:
+            full_source.append(cell['source'])
+    full_source = '\n'.join(full_source)
+    doc = Document(page_content=full_source)
+    doc.metadata['source'] = file_path
+    doc.metadata['parent_folder'] = '/'.join(file_path.split('/')[1:-1])
+    return doc
+
+
+def load_multiple_notebooks(source_dir: str, exclude_pattern='[!_]') -> List[Document]:
+    ipynb_files = glob.glob(os.path.join(source_dir, f"{exclude_pattern}*.ipynb"), recursive=True)
+    all_files = ipynb_files
+    return [load_notebook(file_path) for file_path in all_files]
+
+def create_readme(repo_name, summary_notebooks, summary_repo):
+    markdown_file = ""
+    markdown_file += f"# {repo_name}\n"
+    markdown_file += f'{summary_repo}\n\nNotebooks info:\n'
+    markdown_file += f'{summary_notebooks}'
+    return markdown_file
+
+
+def format_summary(summary_notebooks_results, repo_name):
+    summary_notebooks = '\n'
+    for parent_folder in summary_notebooks_results.keys():
+        llm_results = list(summary_notebooks_results[parent_folder].values())
+        parent_folder = parent_folder.replace(f'{repo_name}/', '')
+        summary_notebooks += f'**{parent_folder}** folder:'
+        summary_notebooks += ''.join(llm_results) + '\n\n'
+    return summary_notebooks
+
+
+def clone_repository(repo_username, repo_name, branch, token=None):
+    git_url = f'https://github.com/{repo_username}/{repo_name}'
+    g = Git(repo_name)
+    if token:
+        git_url = git_url.replace('https://', f'https://{token}@')
+
+    repo_folder = 'repositories/' +repo_name
+    if not os.path.exists(repo_folder):
+        os.makedirs(repo_folder)
+
+    try:
+        repo = Repo.clone_from(git_url, repo_folder, branch=branch)
+    except:
+        repo = Repo(repo_folder)
+        actual_branch = repo.active_branch.name
+        if branch != repo.active_branch.name:
+            repo.git.checkout(branch)
+            print(f'Checkout {actual_branch}->{branch}')
+        else:
+            print(f'Repository {repo_name} ({actual_branch}) already exists')
+    return repo_folder
+
+def get_branches(user, repo, token):
+    headers = {'Accept': 'application/vnd.github+json'}
+    if token:
+        headers['Authorization'] = f'token {token}'
+
+    response = requests.get(f'https://api.github.com/repos/{user}/{repo}/branches', headers=headers)
+
+    branches = [branch['name'] for branch in response.json()]
+    return branches
+
+
+def generate_fake_responses(documents):
+    responses = {}
+    import random
+    for doc in documents:
+        var = 'far' if random.randint(1,100) > 50 else 'bar'
+        notebook_name = doc.metadata['source'].split('/')[-1]
+        responses[notebook_name] = f'\n - `{notebook_name}`: This notebooks is used for {var}'
+    return responses
+
+
+def retrieve_summary(documents, embeddings, model_type='FakeLLM', print_token_n_costs=False):
+    if model_type == 'FakeLLM':
+        responses = generate_fake_responses(documents)
+    print_token_n_costs=True
+    total_cost = 0
+    results_parent_folder_dict={}
+    for doc in documents:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2048,
+                                                chunk_overlap=128,
+                                                separators =  ["\n\n", "\n"])
+        doc_split = text_splitter.split_documents([doc])
+        search_index = Chroma.from_documents(doc_split, embeddings,)
+        retriever = search_index.as_retriever()
+        notebook_name = doc.metadata['source'].split('/')[-1]
+        parent_folder = doc.metadata['parent_folder']
+        if parent_folder not in results_parent_folder_dict.keys():
+              results_parent_folder_dict[parent_folder] = {}
+        callbacks = [StreamingStdOutCallbackHandler()]
+        if model_type == 'GPT4ALL':
+            llm = GPT4All(model=model_path, n_ctx=1000, backend='gptj', callbacks=callbacks, verbose=False)
+        elif model_type == 'OpenAI':
+            llm = OpenAI(temperature=0, max_tokens=500)
+        elif model_type == 'FakeLLM':
+            llm = FakeListLLM(responses=list(responses.values()))
+        query = f"The following code comes from the {notebook_name} Jupyter Notebook. Your task is to explain for what this code is used for.\
+            Describe this using just one paragraph, includig the location of the input and output files from the notebook.\
+            Use a bullet point for you response, which must be in markdown. You must dont add more information. Here is the initial part of your answer:\
+            - `{notebook_name}`: "
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+        print(f'Working on {parent_folder}/{notebook_name}..')
+        if model_type=='OpenAI' and print_token_n_costs:
+            with get_openai_callback() as cb:
+                res = qa(query)
+                print(cb)
+                total_cost += cb.total_cost
+        elif model_type == 'FakeLLM':
+            res = {'result' : responses[notebook_name]}
+        else:
+            res = qa(query)
+        results_parent_folder_dict[parent_folder][notebook_name] = res['result']
+        
+    if total_cost > 0:
+        print(f'Final cost (USD): ${total_cost}\n')
+    return llm, results_parent_folder_dict
+
+def summary_repo(llm, summary_notebooks, repo_name, model_type='FakeLLM', print_token_n_costs=False):
+
+    query_repo = f"""The following is the summary of a list of notebooks used in a GitHub repository called `{repo_name}`:
+{summary_notebooks}
+What do you think is this repo used for? Don't explain me each notebook, just give me a summary of the repository that could be add to a readme.md file."""
+
+    if model_type=='OpenAI' and print_token_n_costs:
+        with get_openai_callback() as cb:
+            summary_repo = llm(query_repo)
+            print(cb)
+    elif model_type == 'FakeLLM':
+            summary_repo = 'This repo contains notebooks for foo'
+    else:
+        summary_repo = llm(query_repo)
+    return summary_repo
